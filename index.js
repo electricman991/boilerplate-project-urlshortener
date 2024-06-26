281ef04aabb6f3d293f30f80c6c1854aeb748793
electricman991 <braidensommer@yahoo.com>
2024-06-26 00:59:27 -0700

    finished challenge

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser')
+const dns = require('dns')
 
+const postMiddlware = bodyParser.urlencoded({extended: false})
+app.use(postMiddlware)
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -17,6 +21,42 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+const urls = {}
+app.post('/api/shorturl', (req, res) => {
+  const originalurl = req.body.URL
+  
+  try {
+  const hostname  = new URL(originalurl).hostname
+
+  dns.lookup(hostname, (err, address, family) => {
+    if (err) {
+      return res.json({ error: 'invalid url' })
+    }
+  
+    const shortUrl = Object.keys(urls).length + 1
+    urls[shortUrl] = originalurl
+    res.json({original_url: originalurl, short_url: shortUrl})
+  })
+  }
+
+  catch {
+    console.log("error")
+    res.json({error: 'invalid url'})
+  }
+})
+
+app.get('/api/shorturl/:url', (req, res) => {
+  const shortUrl = req.params.url
+  
+  // Check if short URL exists in `urls` object
+  if (urls.hasOwnProperty(shortUrl)) {
+    const originalUrl = urls[shortUrl]
+    res.redirect(originalUrl)
+  } else {
+    res.json({ error: 'Short URL not found' });
+  }
 });
 
 app.listen(port, function() {
